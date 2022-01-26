befc59b135d254a79e0e0a0dad6d85b5e39d79fe
shrey-mehta1 <shreymehta2010@gmail.com>
2022-01-26 18:02:50 -0500

    Testing commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -39,7 +39,7 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add "scoped" attribute to limit CSS to this component only . Trial git -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
